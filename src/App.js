94e82adedabacc94b02f187bfad42f3ef286e338
Haastrup ELijah <haastrupea@gmail.com>
2020-09-26 11:27:09 +0100

    Refactor: move dashboard to pages folder

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
-const Dashboard = lazy(() => import('./dashboard/index'));
-const Login = lazy(() => import('./dashboard/Login'));
-const Register = lazy(() => import('./dashboard/Register'));
+const Dashboard = lazy(() => import('./pages/dashboard/index'));
+const Login = lazy(() => import('./dashboard/pages/Login'));
+const Register = lazy(() => import('./dashboard/pages/Register'));
 
 const App = () => (
   <Router>
